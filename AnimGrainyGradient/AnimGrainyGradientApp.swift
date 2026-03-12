a6e49ce61d6c7235e87a6c9ca510bb6ef1c37dc3
Eldar Krotkov <krotkoves@mail.ru>
2026-03-12 21:09:41 +0300

    Initial Commit

---
+++ AnimGrainyGradient/AnimGrainyGradientApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimGrainyGradientApp.swift
+//  AnimGrainyGradient
+//
+//  Created by Эльдар Кротков on 12.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct AnimGrainyGradientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
